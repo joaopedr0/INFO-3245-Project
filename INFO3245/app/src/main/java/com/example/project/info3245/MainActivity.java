dff7c564a8a7e90c5d60a5d5a85d476adbceaa22
Jun Yu Huang <junyu_huang@hotmail.com>
2018-11-16 22:26:05 -0800

    1.8 testing 1
    -added progress activity
    -added progress option to menu

--- INFO3245/app/src/main/java/com/example/project/info3245/MainActivity.java
+++ INFO3245/app/src/main/java/com/example/project/info3245/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -111,6 +112,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // menu items
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -118,16 +120,30 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    // menu event handlers
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                // open the settings activity
+                // startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                Toast.makeText(this, "you are gay", Toast.LENGTH_SHORT);
+                return true;
+            case R.id.task_progress:
+                // open the progress activity
+                startActivity(new Intent(MainActivity.this, ProgressActivity.class));
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
